c060ea1c470bf6f8f6b5ce51990eabf85cd1e146
Luiz Gustavo <81664581+zVeran@users.noreply.github.com>
2022-10-17 12:14:22 -0300

    Add files via upload

---
+++ busca.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+        <p> Pesquisar
+        <input type="option" value="1"></input>
+        <input type="option" value="2"></input>
+        <input type="button" value="Buscar"></input>
+        </p>
+    </div>
+
+    
+<?php
+
+include 'style.css'; 
+
+?>
+
+</body>
+</html>
